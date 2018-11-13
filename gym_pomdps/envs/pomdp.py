45d6d5528aba93e50a36a5ead33224830b3eba28
abaisero <andrea.baisero@gmail.com>
2018-11-13 16:43:53 -0500

    add discount factor to POMDP environment

--- gym_pomdps/envs/pomdp.py
+++ gym_pomdps/envs/pomdp.py
@@ -19,6 +19,7 @@ class POMDP(gym.Env):
         if model.values == 'cost':
             raise ValueError('Unsupported `cost` values.')
 
+        self.discount = model.discount
         self.state_space = spaces.Discrete(len(model.states))
         self.action_space = spaces.Discrete(len(model.actions))
         self.observation_space = spaces.Discrete(len(model.observations))
