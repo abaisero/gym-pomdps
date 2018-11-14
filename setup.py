e63d827f374294ed2f3c942d9c24569dfbfb88a8
abaisero <andrea.baisero@gmail.com>
2018-11-14 10:20:27 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='gym_pomdps',
     version='0.1.0',
     packages=find_packages(),
     package_data={'': ['*.pomdp']},
-    # install_requires=[
-    #     'gym',
-    #     'numpy',
-    #     'rl_parsers',
-    # ],
+    install_requires=[
+        'gym',
+        'numpy',
+        # 'rl_parsers',
+    ],
+    test_suite='tests',
 )
