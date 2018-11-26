bdd149e79befbe304ac19d80351bba0ee056fbaf
abaisero <andrea.baisero@gmail.com>
2018-11-26 16:10:40 -0500

    move setup dependencies in requirements.txt

--- setup.py
+++ setup.py
@@ -6,10 +6,5 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     package_data={'': ['*.pomdp']},
-    install_requires=[
-        'gym',
-        'numpy',
-        # 'rl_parsers',
-    ],
     test_suite='tests',
 )
