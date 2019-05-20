0f5748a7ce036e1791dd56472d1c74d19059284d
abaisero <andrea.baisero@gmail.com>
2019-05-20 13:16:33 -0400

    add MultiPOMDP _seed and _consistency tests

--- tests/test_multipomdp.py
+++ tests/test_multipomdp.py
@@ -46,5 +46,56 @@ class Gym_MultiPOMDP_Test(unittest.TestCase):
             raise Exception(f'Episodic Environment did not end (dones={dones})')
 
 
+    def test_seed(self):
+        seed = 17
+        ntrajectories, nsteps = 5, 10
+
+        env = gym.make('POMDP-loadunload-v0')
+        env = gym_pomdps.MultiPOMDP(env, ntrajectories)
+        actions = rnd.randint(env.action_space.n, size=(nsteps, ntrajectories))
+
+        # run environment multiple times with same seed
+        outputs = []
+        for _ in range(2):
+            env.seed(seed)
+            env.reset()
+            output = list(map(env.step, actions))
+            outputs.append(output)
+
+        outputs1, outputs2 = outputs
+        for (o1, r1, done1, info1), (o2, r2, done2, info2) in zip(*outputs):
+            np.testing.assert_equal(o1, o2)
+            np.testing.assert_equal(r1, r2)
+            np.testing.assert_equal(done1, done2)
+            self.assertEqual(info1, info2)
+
+
+    def test_consistency(self):
+        seed = 17
+        ntrajectories = 1  # single sample necessary to control randomness
+        nsteps = 10
+
+        env = gym.make('POMDP-loadunload-v0')
+        actions = rnd.randint(env.action_space.n, size=nsteps)
+
+        env.seed(seed)
+        env.reset()
+        outputs1 = list(map(env.step, actions))
+
+        env = gym_pomdps.MultiPOMDP(env, ntrajectories)
+        actions = actions.reshape(-1, 1)
+
+        env.seed(seed)
+        env.reset()
+        outputs2 = list(map(env.step, actions))
+
+        outputs = zip(outputs1, outputs2)
+        for (o1, r1, done1, info1), (o2, r2, done2, info2) in outputs:
+            np.testing.assert_equal(o1, o2)
+            np.testing.assert_equal(r1, r2)
+            np.testing.assert_equal(done1, done2)
+            self.assertEqual(info1, info2)
+
+
 if __name__ == '__main__':
     unittest.main()
