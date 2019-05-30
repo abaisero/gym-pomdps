88bc56efa1fa56c9516c24777d03778dbb33015c
abaisero <andrea.baisero@gmail.com>
2019-05-30 11:27:51 -0400

    add version support, clean shopping.pomdp versions

--- gym_pomdps/__init__.py
+++ gym_pomdps/__init__.py
@@ -1,4 +1,6 @@
+import re
 from pkg_resources import resource_listdir, resource_filename, resource_isdir
+
 from .envs import POMDP
 from .wrappers import MultiPOMDP
 from .envs.registration import register, env_list
@@ -16,16 +18,22 @@ def is_pomdp(name):
 fnames = filter(is_pomdp, resource_listdir('gym_pomdps.pomdps', ''))
 for fname in fnames:
     fpath = resource_filename('gym_pomdps.pomdps', fname)
-    name = '.'.join(fname.split('.')[:-1])
+    name = '.'.join(fname.split('.')[:-1])  # remove .pomdp suffix
+    version = 0
+
+    # extract version if any
+    m = re.fullmatch('(?P<name>.*)\.v(?P<version>\d+)', name)
+    if m is not None:
+        name, version = m['name'], m['version']
 
     register(
-        id=f'POMDP-{name}-v0',
+        id=f'POMDP-{name}-v{version}',
         entry_point='gym_pomdps.envs:POMDP',
         kwargs=dict(path=fpath, episodic=False),
     )
 
     register(
-        id=f'POMDP-{name}-episodic-v0',
+        id=f'POMDP-{name}-episodic-v{version}',
         entry_point='gym_pomdps.envs:POMDP',
         kwargs=dict(path=fpath, episodic=True),
     )
